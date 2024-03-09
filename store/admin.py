8fbcd3053686b7d4f7abeca8b9f5516cba9a70c6
Md Ahaduzzaman <mdahaduzzamanhridoy@gmail.com>
2024-03-09 20:30:48 +0600

    Store, Product, ShopAdmin, ProductImage, category Model added to the store app

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'owner', 'is_active']
+    
+    def delete_queryset(self, request, queryset):
+        """To delete the respective image from the storage"""
+        for obj in queryset:
+            obj.delete()
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'is_active']
+    
+    def delete_queryset(self, request, queryset):
+        """To delete the respective image from the storage"""
+        for obj in queryset:
+            obj.delete()
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'category', 'is_active']
+    
+    def delete_queryset(self, request, queryset):
+        """To delete the respective image from the storage"""
+        for obj in queryset:
+            obj.delete()
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'is_active']
+    
+    def delete_queryset(self, request, queryset):
+        """To delete the respective image from the storage"""
+        for obj in queryset:
+            obj.delete()
